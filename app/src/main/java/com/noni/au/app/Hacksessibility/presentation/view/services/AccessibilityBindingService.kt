4dae9fab0ee9a999e06161c5f48532c47c7a8272
Harsh Overseer <harsh@embry.io>
2017-10-25 21:10:37 +1100

    adds notification.

--- app/src/main/java/com/noni/au/app/Hacksessibility/presentation/view/services/AccessibilityBindingService.kt
+++ app/src/main/java/com/noni/au/app/Hacksessibility/presentation/view/services/AccessibilityBindingService.kt
@@ -1,9 +1,14 @@
 package com.noni.au.app.Hacksessibility.presentation.view.services
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import android.support.v4.app.NotificationCompat
 import android.util.Log
+import com.noni.au.app.Hacksessibility.R
+import com.noni.au.app.Hacksessibility.presentation.view.activities.MainActivity
 
 /**
  * ideally this service will
@@ -21,13 +26,51 @@ class AccessibilityBindingService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.v("TAG", "service started")
-        stopSelf()
+        createNotification()
+        //stopSelf()
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.v("TAG", "service finishing up..")
+    }
+
+    //endregion
+
+    //region private
+
+    private fun createNotification() {
+        val id = resources.getString(R.string.NOTIFICATION_CHANNEL_ID)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+
+
+        val builder = NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.img_dot_higlighted)
+                .setContentTitle(resources.getString(R.string.text_channel_name))
+                .setContentText(resources.getString(R.string.text_channel_description))
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = resources.getString(R.string.text_channel_name)
+            val description = resources.getString(R.string.text_channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance)
+            channel.description = description
+            manager?.createNotificationChannel(channel)
+        }
+
+
+
+        val resultIntent = Intent(this, MainActivity::class.java)
+
+        val stackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntent(resultIntent)
+
+        val pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        builder.setContentIntent(pendingIntent)
+
+        manager?.notify(R.string.NOTIFICATION_CHANNEL_ID, builder.build())
     }
 
     //endregion
